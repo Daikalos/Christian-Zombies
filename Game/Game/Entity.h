8a015a60ac1c7e5ab6a148aa25ff0a8e3be5061b
Nox <nemowir3n@gmail.com>
2018-11-21 13:12:43 +0100

    Entity Base Updated
    ~Entity Base fixed
    ~Updated Enemy.cpp code
    ~Updated Player.cpp code

--- Game/Game/Entity.h
+++ Game/Game/Entity.h
@@ -1,6 +1,9 @@
 #ifndef ENTITY_HEADER
 #define ENTITY_HEADER
 
+#include "stdafx.h"
+#include <SFML/Graphics.hpp>
+
 class Entity {
 public:
 	float GetHealthPoints() const;
@@ -12,18 +15,19 @@ public:
 	void SetDamageValue(float aDamageValue);
 
 	virtual void Init();
-	virtual void Update();
-	virtual void Draw();
+	virtual void Update(const float &aDeltaTimeValue);
+	virtual void Draw(sf::RenderWindow *aWindow);
 
 	virtual void Move();
 	virtual void Attack();
 protected:
+	float myDeltaTime;
+
 	float myHealthPoints;
 	float myMaxHealth;
 
 	float myDamageValue;
 
 	void SetMaxHealth(float aMaxHealthValue);
-}
-
+};
 #endif
